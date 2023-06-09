672164fe2f3b559577957184ad8a45f48e6715af
Jahid Eljah <jahideljah@gmail.com>
2023-06-09 21:23:21 +0200

    FizzBuzz challenge functional solution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////
-////////////* FizzBuzz Practice *////////////
+///////////* FizzBuzz challenge *////////////
 /////////////////////////////////////////////
 
 for (let i = 1; i <= 100; i++) {
@@ -13,3 +13,22 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+
+// FizzBuzz challenge functional solution: //
+
+function fizzBuzz() {
+  let output = "";
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output += "FizzBuzz\n";
+    } else if (i % 3 === 0) {
+      output += "Fizz\n";
+    } else if (i % 5 === 0) {
+      output += "Buzz\n";
+    } else {
+      output += i + "\n";
+    }
+  }
+  return output;
+}
+console.log(fizzBuzz());
