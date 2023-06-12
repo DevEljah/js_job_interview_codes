86519ede8da4a01c90cf170ed95af1c101608b16
Jahid Eljah <jahideljah@gmail.com>
2023-06-12 21:12:55 +0200

    Logical operators

--- index.js
+++ index.js
@@ -57,3 +57,48 @@ console.log(z); */
 var obj2 = obj1;
 obj1.name = "Dev";
 console.log(obj2); */
+
+/////////////////////////////////////////////
+/////////// Logical operators: //////////////
+/////////////////////////////////////////////
+// Logical operators in javascript, unlike operators in other programming languages,
+// do not return true or false. They always return one of the operands.
+
+// OR ( || ) operator - If the first value is truthy, then the first value is returned.
+// Otherwise, always the second value gets returned.
+
+// AND ( && ) operator - If both the values are truthy, always the second value is returned.
+// If the first value is falsy then the first value is returned
+// or if the second value is falsy then the second value is returned.
+
+// Example:
+
+var a = 220;
+var b = "Hi";
+var c = undefined;
+
+a || b; // Returns 220 since the first value is truthy
+
+a || c; // Returns 220 since the first value is truthy
+
+a && b; // Returns "Hi" since both the values are truthy
+
+b && c; // Returns undefined since the second value is falsy
+
+if (a || c) {
+  console.log("Code runs"); // This block runs because a || b returns 220 (Truthy)
+}
+
+if (a && b) {
+  console.log("Code runs"); // This block runs because a && b returns "Hi" (Truthy)
+}
+
+function myFunction() {
+  var x = 5;
+  // debugger; // Execution pauses here
+  console.log(x);
+}
+
+myFunction();
+
+console.log("deb");
